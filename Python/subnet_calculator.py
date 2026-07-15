ab0ef596c0e9d969c86c5b228033e5f70d982567
Princewill Agbodike <princewillagbodike@gmail.com>
2026-07-15 18:14:08 +0200

    Update subnet_calculator.py

--- Python/subnet_calculator.py
+++ Python/subnet_calculator.py
@@ -6,3 +6,10 @@ print("Network Address :", network.network_address)
 print("Broadcast Address :", network.broadcast_address)
 print("Subnet Mask :", network.netmask)
 print("Number of Hosts :", network.num_addresses)
+
+
+# sample output
+Network Address : 192.168.10.0
+Broadcast Address : 192.168.10.255
+Subnet Mask : 255.255.255.0
+Number of Hosts : 256
