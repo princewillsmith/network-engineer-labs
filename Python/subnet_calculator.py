688758a686c1ba39379e74dbb94ad103c0f79127
Princewill Agbodike <princewillagbodike@gmail.com>
2026-07-15 18:13:11 +0200

    Update subnet_calculator.py

--- Python/subnet_calculator.py
+++ Python/subnet_calculator.py
@@ -1,1 +1,8 @@
+import ipaddress
 
+network = ipaddress.ip_network("192.168.10.0/24")
+
+print("Network Address :", network.network_address)
+print("Broadcast Address :", network.broadcast_address)
+print("Subnet Mask :", network.netmask)
+print("Number of Hosts :", network.num_addresses)
